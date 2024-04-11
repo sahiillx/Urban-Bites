98fe5d243e6cfb9e2e53b43e081a1f6ac23de77c
Sahil Khan <sahilkhan0471@gmail.com>
2024-04-11 17:09:40 +0530

    initial commit

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+import {assets} from "../../assets/assets"
+
+const Nav = styled.div``;
+
+const Navbar = () => {
+  return (
+    <Nav>
+        <Logo src/>
+    </Nav>
+
+    
+  )
+}   
+
+export default Navbar
