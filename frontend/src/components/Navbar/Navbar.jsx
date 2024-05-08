5d1cb3f97cf6b81613c32ec7c2d0db498b326111
Sahil Khan <sahilkhan0471@gmail.com>
2024-05-08 15:02:22 +0530

    made navbar responsive

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -10,7 +10,15 @@ const Nav = styled.div`
 `;
 
 const Logo = styled.img`
-width: 150px;
+  width: 150px;
+
+  @media (max-width: 1050px) {
+    width: 140px;
+  }
+  @media (max-width: 900px) {
+    width: 120px;
+  }
+  
 `;
 const NavMenu = styled.ul`
   display: flex;
@@ -18,7 +26,6 @@ const NavMenu = styled.ul`
   gap: 20px;
   color: #49557e;
   font-size: 18px;
-  
 
   li {
     cursor: pointer;
@@ -27,16 +34,40 @@ const NavMenu = styled.ul`
   .active {
     padding-bottom: 2px;
     border-bottom: 2px solid #49557e;
-    
-  
+  }
+
+  @media (max-width: 1050px) {
+    gap: 18px;
+    font-size: 17px;
+  }
+  @media (max-width: 900px) {
+    gap: 15px;
+    font-size: 16px;
+  }
+  @media (max-width: 760px) {
+    display: none;
   }
 `;
 const NavRight = styled.div`
   display: flex;
   align-items: center;
   gap: 40px;
+
+  @media (max-width: 1050px) {
+    gap: 30px;
+  }
+  @media (max-width: 900px) {
+    gap: 20px;
+  }
 `;
-const Img = styled.img``;
+const Img = styled.img`
+  @media (max-width: 1050px) {
+    width: 22px;
+  }
+  @media (max-width: 900px) {
+    width: 20px;
+  }
+`;
 const NavSearch = styled.div`
   position: relative;
 `;
@@ -61,19 +92,48 @@ const Button = styled.button`
 
   &:hover {
     background-color: #fff4f2;
-   }
+  }
+
+  @media (max-width: 1050px) {
+    padding: 8px 25px;
+  }
+  @media (max-width: 900px) {
+    padding: 7px 20px;
+    font-size: 15px;
+
+  }
 `;
 
 const Navbar = () => {
-  const [menu , setMenu] = React.useState("Home");
+  const [menu, setMenu] = React.useState("Home");
   return (
     <Nav>
       <Logo src={assets.logo} />
       <NavMenu>
-        <li onClick={()=>setMenu("Home")} className={menu ==="Home" ? "active" : ""}>Home</li>
-        <li onClick={()=>setMenu("Menu")} className={menu ==="Menu" ? "active" : ""}>Menu</li>
-        <li onClick={()=>setMenu("Mobile-App")} className={menu ==="Mobile-App" ? "active" : ""}>Mobile-App</li>
-        <li onClick={()=>setMenu("Contact-Us")} className={menu ==="Contact-Us" ? "active" : ""}>Contact us</li>
+        <li
+          onClick={() => setMenu("Home")}
+          className={menu === "Home" ? "active" : ""}
+        >
+          Home
+        </li>
+        <li
+          onClick={() => setMenu("Menu")}
+          className={menu === "Menu" ? "active" : ""}
+        >
+          Menu
+        </li>
+        <li
+          onClick={() => setMenu("Mobile-App")}
+          className={menu === "Mobile-App" ? "active" : ""}
+        >
+          Mobile-App
+        </li>
+        <li
+          onClick={() => setMenu("Contact-Us")}
+          className={menu === "Contact-Us" ? "active" : ""}
+        >
+          Contact us
+        </li>
       </NavMenu>
       <NavRight>
         <Img src={assets.search_icon} />
